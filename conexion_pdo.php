a7f4c80a81595c07689a8d2585486588f0679ac4
gabijrey <gabrielreyvilchez@gmail.com>
2026-03-17 19:39:00 +0100

    code (creación del archivo conexion.php) Crear conexion PDO para la base de datos

---
+++ conexion_pdo.php
@@ -0,0 +1,17 @@
+<?php
+$_servidor = "localhost";
+$_bd = "comiclook";
+$_usuario = "ADMIN";
+$_contrasena = "ADMIN";
+
+try {
+    $_conexion = new PDO(
+        "mysql:host=$_servidor;dbname=$_bd;charset=utf8mb4",
+        $_usuario,
+        $_contrasena
+    );
+    $_conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $_conexion->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+} catch (PDOException $e) {
+    die();
+}
