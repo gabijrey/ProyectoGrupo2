11430511c3c85e29daf9a25cb380aa249f23b53b
isralfp14 <isralozano14@gmail.com>
2026-04-14 21:38:56 +0200

    code (inserts con rutas actualizadas)

--- insert_og.sql
+++ insert_og.sql
@@ -12,24 +12,24 @@ Basicamente el proposito de este archivo es que se vean las portadas que ya tene
 -- 3. INSERT DE 4 CÓMICS (Tipo 0)
 -- Usamos 'Norma Editorial' para la mayoría de cómics/manga en España
 INSERT INTO obra (titulo, descripcion, genero, anno_lanzamiento, portada, tipo, nombre_editorial) VALUES 
-('Soy un ángel perdido', 'Una historia tierna sobre los lazos familiares y el destino.', 'Drama', 2026, 'imagen/angel_perdido.jpg', 0, 'Norma Editorial'),
-('Carta Blanca', 'Un romance contado de forma inversa, desde el final hasta el principio.', 'Romance', 2021, 'imagen/carta_blanca.jpg', 0, 'Norma Editorial'),
-('Batman: Hush', 'Uno de los misterios más grandes del Caballero Oscuro.', 'Superhéroes', 2002, 'batman_hush.jpg', 0, 'Norma Editorial'),
-('Justice League: Origin', 'El renacimiento de la Liga de la Justicia contra Darkseid.', 'Superhéroes', 2011, 'imagen/jl_origin.jpg', 0, 'Norma Editorial');
+('Soy un ángel perdido', 'Una historia tierna sobre los lazos familiares y el destino.', 'Drama', 2026, 'imagen/img_obras/comics/angel_perdido.jpg', 0, 'Norma Editorial'),
+('Carta Blanca', 'Un romance contado de forma inversa, desde el final hasta el principio.', 'Romance', 2021, 'imagen/img_obras/comics/carta_blanca.jpg', 0, 'Norma Editorial'),
+('Batman: Hush', 'Uno de los misterios más grandes del Caballero Oscuro.', 'Superhéroes', 2002, 'imagen/img_obras/comics/batman_hush.jpg', 0, 'Norma Editorial'),
+('Justice League: Origin', 'El renacimiento de la Liga de la Justicia contra Darkseid.', 'Superhéroes', 2011, 'imagen/img_obras/comics/jl_origin.jpg', 0, 'Norma Editorial');
 
 
 -- 4. INSERT DE 4 LIBROS (Tipo 2)
 -- Usamos 'B de Bolsillo' y 'Debolsillo' para narrativa
 INSERT INTO obra (titulo, descripcion, genero, anno_lanzamiento, portada, tipo, nombre_editorial) VALUES 
-('Project Hail Mary', 'Un hombre despierta en una nave espacial sin recordar quién es.', 'Ciencia Ficción', 2021, 'imagen/hail_mary.jpg', 2, 'B de Bolsillo'),
-('The Martian', 'Un astronauta queda atrapado en Marte y debe sobrevivir con su ingenio.', 'Ciencia Ficción', 2011, 'imagen/martian.jpg', 2, 'B de Bolsillo'),
-('El Imperio Final', 'Un mundo donde el héroe profetizado falló y el mal gobierna.', 'Fantasía', 2006, 'mistborn.jpg', 2, 'Debolsillo'),
-('El Camino de los Reyes', 'El inicio de la épica saga del Archivo de las Tormentas.', 'Fantasía', 2010, 'imagen/way_of_kings.jpg', 2, 'B de Bolsillo');
+('Project Hail Mary', 'Un hombre despierta en una nave espacial sin recordar quién es.', 'Ciencia Ficción', 2021, 'imagen/img_obras/libros/hail_mary.jpg', 2, 'B de Bolsillo'),
+('The Martian', 'Un astronauta queda atrapado en Marte y debe sobrevivir con su ingenio.', 'Ciencia Ficción', 2011, 'imagen/img_obras/libros/martian.jpg', 2, 'B de Bolsillo'),
+('El Imperio Final', 'Un mundo donde el héroe profetizado falló y el mal gobierna.', 'Fantasía', 2006, 'imagen/img_obras/libros/mistborn.jpg', 2, 'Debolsillo'),
+('El Camino de los Reyes', 'El inicio de la épica saga del Archivo de las Tormentas.', 'Fantasía', 2010, 'imagen/img_obras/libros/way_of_kings.jpg', 2, 'B de Bolsillo');
 
 
 -- 5. INSERT DE 4 MANGAS (Tipo 1)
 INSERT INTO obra (titulo, descripcion, genero, anno_lanzamiento, portada, tipo, nombre_editorial) VALUES 
-('Jujutsu Kaisen Vol. 1', 'Un joven se traga un dedo maldito y entra en el mundo de los hechiceros.', 'Shonen', 2018, 'imagen/jjk_1.jpg', 1, 'Norma Editorial'),
-('Jujutsu Kaisen 0', 'La precuela que explica el origen de las maldiciones más fuertes.', 'Shonen', 2017, 'imagen/jjk_0.jpg', 1, 'Norma Editorial'),
-('Jujutsu Kaisen Vol. 26', 'La batalla culminante en Shinjuku llega a su punto crítico.', 'Shonen', 2024, 'imagen/jjk_26.jpg', 1, 'Norma Editorial'),
-('All You Need is Kill', 'Un soldado atrapado en un bucle temporal luchando contra aliens.', 'Seinen', 2014, 'imagen/all_kill.jpg', 1, 'Norma Editorial');
+('Jujutsu Kaisen Vol. 1', 'Un joven se traga un dedo maldito y entra en el mundo de los hechiceros.', 'Shonen', 2018, 'imagen/img_obras/mangas/jjk_1.jpg', 1, 'Norma Editorial'),
+('Jujutsu Kaisen 0', 'La precuela que explica el origen de las maldiciones más fuertes.', 'Shonen', 2017, 'imagen/img_obras/mangas/jjk_0.jpg', 1, 'Norma Editorial'),
+('Jujutsu Kaisen Vol. 26', 'La batalla culminante en Shinjuku llega a su punto crítico.', 'Shonen', 2024, 'imagen/img_obras/mangas/jjk_26.jpg', 1, 'Norma Editorial'),
+('All You Need is Kill', 'Un soldado atrapado en un bucle temporal luchando contra aliens.', 'Seinen', 2014, 'imagen/img_obras/mangas/all_kill.jpg', 1, 'Norma Editorial');
